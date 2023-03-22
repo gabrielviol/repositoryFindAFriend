95a4da8c8f3a8f585ba5b8dff4d9c7ef0cc4797f
Gabriel Oliveira <gabrielviol00@hotmail.com>
2023-03-22 14:29:46 -0300

    up

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,23 +16,29 @@ export function Home() {
     // TO DO
   }
 
-  return <Container>
-    <Content>
-      <Header>
-        <img src={logo} alt="Logo Find A Friend" />
-        <span>FindAFriend</span>
-      </Header>
-      <Body>
-        <h1>Leve <br/> a felicidade <br/> para o seu lar</h1>
-        <img src={banner} alt="banner" />
-      </Body>
-      <Search>
-        <span>Encontre o animal de estimação ideal <br/> para seu estilo de vida!</span>
-        <div>
-          <p>Busque um amigo: </p>
-          <Select label='Estado' name='Estado' options={[{label: 'a', value: 1}, {label: 'b', value: 2}, {label: 'c', value: 3}]} />
-        </div>
-      </Search>
-    </Content>
-  </Container>
+  return (
+    <Container>
+      <Content>
+        <Header>
+          <img src={logo} alt="Logo Find A Friend" />
+          <span>FindAFriend</span>
+        </Header>
+        <Body>
+          <h1>Leve <br /> a felicidade <br /> para o seu lar</h1>
+          <img src={banner} alt="banner" />
+        </Body>
+        <Search>
+          <span>Encontre o animal de estimação ideal <br /> para seu estilo de vida!</span>
+          <div>
+            <p>Busque um amigo: </p>
+            <Select label='' name='UF' options={states} onChange={handleChangeState} />
+            <Select label='' name='Cidade' options={citys} onChange={handleChangeCity} />
+            <Button onClick={handleSearchPets} disabled={!state || !city}>
+              <img src={searchIcon} alt="" />
+            </Button>
+          </div>
+        </Search>
+      </Content>
+    </Container>
+  )
 }
