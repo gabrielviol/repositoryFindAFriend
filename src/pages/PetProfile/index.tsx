f3f56cdb2a449311267797cc3c65889e0c27258f
Gabriel Oliveira <gabrielviol00@hotmail.com>
2023-03-29 12:00:08 -0300

    create constants

--- src/pages/PetProfile/index.tsx
+++ src/pages/PetProfile/index.tsx
@@ -1,7 +1,20 @@
+import { Sidebar } from "@/components/Sidebar";
 import { useCoordinates } from "@/hooks/use-location";
 import { usePetGallery, usePetRequirements, usePetsDetail } from "@/hooks/use-pet";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { Container, Content, Header } from "./styles";
+
+type PetProfileParams = {
+    id: string
+}
+
+const MapIcon = Leaflet.icon({
+    iconUrl: mapMarker,
+    iconSize: [ 64, 72 ],
+    iconAnchor: [ 32, 72 ],
+    popupAnchor: [ 0, -72 ]
+})
 
 export function PetProfile(){
     const params = useParams<PetProfileParams>()
