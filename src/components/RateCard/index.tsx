fb804815e05fc06a17aab28802999cc1658363f0
Gabriel Oliveira <gabrielviol00@hotmail.com>
2023-03-29 17:30:01 -0300

    create component RateCard

---
+++ src/components/RateCard/index.tsx
@@ -0,0 +1,31 @@
+interface RateCardProps {
+    label: string
+    maxRate: number
+    initialRate: number
+    rateOnSymbol: string
+    rateOffSymbol: string
+}
+
+export function RateCard({
+    initialRate,
+    label,
+    maxRate,
+    rateOffSymbol,
+    rateOnSymbol
+}: RateCardProps){
+    const rate = Array(maxRate)
+        .fill(0)
+        .map((_, index) => {
+            return index < initialRate ? rateOnSymbol : rateOffSymbol
+        })
+        return (
+            <RateContainer>
+                <RateSymbols>
+                    {rate.map((symbol, index) => {
+                        return <img key={String(index)} src={symbol} alt=""/>
+                    })}
+                </RateSymbols>
+                <RateLabel>{label}</RateLabel>
+            </RateContainer>
+        )
+}
