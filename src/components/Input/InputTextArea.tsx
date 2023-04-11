e53257e2af8c6005e6f2319d09b8a8dc5962e8fe
gabrielviol <gabrielviol@hotmail.com>
2023-04-11 14:57:01 -0300

    cadastro de pet pelas organizações

---
+++ src/components/Input/InputTextArea.tsx
@@ -0,0 +1,29 @@
+import { TextareaHTMLAttributes, forwardRef } from "react";
+import { Container, ErrorMessage, InputWrapper } from "./styles";
+
+interface InputTextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement>{
+    label: string
+    labelExtraText?: string
+    errorMessage?: string
+}
+
+export const InputTextArea = forwardRef<any, InputTextAreaProps>(
+    ({label, labelExtraText = '', errorMessage, ...rest}, ref) => {
+        return (
+            <>
+                <Container>
+                    <label>
+                        {label}
+                        <span>{labelExtraText}</span>
+                    </label>
+                    <InputWrapper>
+                        <textarea id={rest.name} ref={ref} {...rest} />
+                    </InputWrapper>
+                    {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+                </Container>
+            </>
+        )
+    }
+)
+
+InputTextArea.displayName = 'InputTextArea'
